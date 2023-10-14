d4c3a19df4d7f164002b678afb702a12da5538b2
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-10-14 15:57:45 +0530

    Update: added links to github repo and replit file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,21 +4,38 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="nav_wrapper">
-            <Link to="/" className="nav_links">
-                Inventory
-            </Link>
-            <Link to="/sales" className="nav_links">
-                Sales
-            </Link>
-            <Link to="/reports-analytics" className="nav_links">
-                Reports
-            </Link>
-            {/* <Link to="/foodItems" className="nav_links">
-                FoodItems
-            </Link>
-            <Link to="/goals" className="nav_links">
-                Goals
-            </Link> */}
+            <div className="centered_links">
+                <Link to="/" className="nav_links">
+                    Inventory
+                </Link>
+                <Link to="/sales" className="nav_links">
+                    Sales
+                </Link>
+                <Link to="/reports-analytics" className="nav_links">
+                    Reports
+                </Link>
+            </div>
+
+            <div className="end_links">
+                <a
+                    href="https://github.com/rushikesh1799/stock-inventory-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="nav_links"
+                    style={{ marginRight: "8px" }}
+                >
+                    Github
+                </a>
+
+                <a
+                    href="https://replit.com/@RushikeshBunge1/Inventory-Management-backend-API#index.js"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="nav_links"
+                >
+                    Replit
+                </a>
+            </div>
         </nav>
     );
 };
